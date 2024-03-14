4f57f73e023114910997ccc7075381b3ad186653
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2024-03-14 16:11:54 +0800

    init project

--- src/plugin-main.cpp
+++ src/plugin-main.cpp
@@ -16,20 +16,16 @@ You should have received a copy of the GNU General Public License along
 with this program. If not, see <https://www.gnu.org/licenses/>
 */
 
+#include <QtWidgets>
 #include <obs-module.h>
 #include <plugin-support.h>
 
 OBS_DECLARE_MODULE()
 OBS_MODULE_USE_DEFAULT_LOCALE(PLUGIN_NAME, "en-US")
 
-bool obs_module_load(void)
-{
-	obs_log(LOG_INFO, "plugin loaded successfully (version %s)",
-		PLUGIN_VERSION);
-	return true;
+bool obs_module_load(void) {
+  obs_log(LOG_INFO, "plugin loaded successfully (version %s)", PLUGIN_VERSION);
+  return true;
 }
 
-void obs_module_unload(void)
-{
-	obs_log(LOG_INFO, "plugin unloaded");
-}
+void obs_module_unload(void) { obs_log(LOG_INFO, "plugin unloaded"); }
